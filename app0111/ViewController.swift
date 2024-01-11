68bb540c1f3fd2fcc5724bb610b7e11e0a71dbfd
jjarajjara <jjarajjara1903@naver.com>
2024-01-11 16:55:15 +0900

    Initial Commit

---
+++ app0111/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  app0111
+//
+//  Created by 김태진 on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var IbIHello: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
